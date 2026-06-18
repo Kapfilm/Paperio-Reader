ea729327186c064a3644d8673f68660d48e634e2
jpirnay <jens@pirnay.com>
2026-06-18 23:06:59 +0200

    Slight adjustment settings menu to fit one page

--- src/SettingsList.h
+++ src/SettingsList.h
@@ -130,10 +130,12 @@ inline std::vector<SettingInfo> buildSettingsList() {
   settings.push_back(SettingInfo::Action(StrId::STR_REFRESH_FREQ, SettingAction::RefreshFrequencyPicker)
                          .withDisplayGetter(getRefreshFrequencyDisplay)
                          .withCategory(StrId::STR_CAT_DISPLAY)
+                         .withSubmenu(StrId::STR_MENU_DISP_REFRESH)
                          .withSubcategory(StrId::STR_MENU_DISP_REFRESH));
   settings.push_back(SettingInfo::Toggle(StrId::STR_REFRESH_AFTER_IMAGE_PAGES,
                                          &CrossPointSettings::halfRefreshAfterImagePage, "halfRefreshAfterImagePage",
                                          StrId::STR_CAT_DISPLAY)
+                         .withSubmenu(StrId::STR_MENU_DISP_REFRESH)
                          .withSubcategory(StrId::STR_MENU_DISP_REFRESH));
   settings.push_back(SettingInfo::Toggle(StrId::STR_SUNLIGHT_FADING_FIX, &CrossPointSettings::fadingFix, "fadingFix",
                                          StrId::STR_CAT_DISPLAY));
@@ -342,9 +344,11 @@ inline std::vector<SettingInfo> buildSettingsList() {
                          .withDeviceTarget(SettingDeviceTarget::X3));
   // --- System ---
   settings.push_back(SettingInfo::Toggle(StrId::STR_SHOW_HIDDEN_FILES, &CrossPointSettings::showHiddenFiles,
-                                         "showHiddenFiles", StrId::STR_CAT_SYSTEM));
+                                         "showHiddenFiles", StrId::STR_CAT_SYSTEM)
+                                         .withSubmenu(StrId::STR_SHOW_FILES));
   settings.push_back(SettingInfo::Toggle(StrId::STR_SHOW_FILE_EXTENSIONS, &CrossPointSettings::showFileExtensions,
-                                         "showFileExtensions", StrId::STR_CAT_SYSTEM));
+                                         "showFileExtensions", StrId::STR_CAT_SYSTEM) 
+                                         .withSubmenu(StrId::STR_SHOW_FILES));
   settings.push_back(SettingInfo::Toggle(StrId::STR_INCLUDE_BETA_UPDATES, &CrossPointSettings::includeBetaUpdates,
                                          "includeRcUpdates", StrId::STR_CAT_SYSTEM));
   // Will be dealt with separately, so these receive none of the main categories and
@@ -368,30 +372,30 @@ inline std::vector<SettingInfo> buildSettingsList() {
   settings.push_back(SettingInfo::DynamicString(
       StrId::STR_SYNC_SERVER_URL, static_cast<SettingInfo::StringGetterFn>(getKoReaderServerUrl),
       [](void*, const std::string& v) {
-        KOREADER_STORE.setServerUrl(v);
-        KOREADER_STORE.saveToFile();
+    KOREADER_STORE.setServerUrl(v);
+    KOREADER_STORE.saveToFile();
       },
       "koServerUrl", StrId::STR_KOREADER_SYNC));
   settings.push_back(SettingInfo::DynamicString(
       StrId::STR_KOREADER_USERNAME, static_cast<SettingInfo::StringGetterFn>(getKoReaderUsername),
       [](void*, const std::string& v) {
-        KOREADER_STORE.setCredentials(v, KOREADER_STORE.getPassword());
-        KOREADER_STORE.saveToFile();
+    KOREADER_STORE.setCredentials(v, KOREADER_STORE.getPassword());
+    KOREADER_STORE.saveToFile();
       },
       "koUsername", StrId::STR_KOREADER_SYNC));
   settings.push_back(SettingInfo::DynamicString(
                          StrId::STR_KOREADER_PASSWORD, static_cast<SettingInfo::StringGetterFn>(getKoReaderPassword),
                          [](void*, const std::string& v) {
-                           KOREADER_STORE.setCredentials(KOREADER_STORE.getUsername(), v);
-                           KOREADER_STORE.saveToFile();
+    KOREADER_STORE.setCredentials(KOREADER_STORE.getUsername(), v);
+    KOREADER_STORE.saveToFile();
                          },
                          "koPassword", StrId::STR_KOREADER_SYNC)
                          .withObfuscated());
   settings.push_back(SettingInfo::DynamicEnum(
       StrId::STR_DOCUMENT_MATCHING, {StrId::STR_FILENAME, StrId::STR_BINARY}, getKoReaderMatchMethod,
       [](void*, uint8_t v) {
-        KOREADER_STORE.setMatchMethod(static_cast<DocumentMatchMethod>(v));
-        KOREADER_STORE.saveToFile();
+    KOREADER_STORE.setMatchMethod(static_cast<DocumentMatchMethod>(v));
+    KOREADER_STORE.saveToFile();
       },
       "koMatchMethod", StrId::STR_KOREADER_SYNC));
   settings.push_back(SettingInfo::Toggle(StrId::STR_KO_SYNC_ON_BOOK_CLOSE, &CrossPointSettings::koSyncOnBookClose,
